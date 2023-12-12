bdb111295c7c136f9ef2c1f79a883235fe3b1ccd
Htet Aung Khant <bob.h@bettercast.io>
2023-12-12 13:49:43 +0630

    first commit

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO.Compression;
+
+namespace GGWP{
+    class WPGG{
+        public static void Main(string[] args){
+            // declare int a, and b to operate
+            int a,b;
+
+            //string template to prompt number
+            string num_prompt = "Type a number, and then press Enter";
+            Console.WriteLine(num_prompt);
+
+            //get a
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(num_prompt);
+
+            //get b
+            b = Convert.ToInt32(Console.ReadLine());
+
+            //prompt operand (+,-,*,/)
+            Console.WriteLine("Choose an option from the following list:\n\ta - Add\n\ts - Substract\n\tm - Multiplication\n\td - Division");
+
+            switch(Console.ReadLine()){
+                case "a":
+                    Console.WriteLine($"Your Result: {a} + {b} = {a+b}");
+                    break;
+                case "s":
+                    Console.WriteLine($"Your Result: {a} - {b} = {a-b}");
+                    break;
+                case "m":
+                    Console.WriteLine($"Your Result: {a} * {b} = {a*b}");
+                    break;
+                case "d":
+                    Console.WriteLine($"Your Result: {a} / {b} = {a/b}");
+                    break;
+                default:
+                    Console.WriteLine("Are you too stupid to read that simple instructions?");
+                    break;
+            }
+        }
+        
+    }
+}
---
+++ task.md
@@ -0,0 +1,8 @@
+# Task
+Write a c# console program to grade a student, using the mark input from console.
+1. if the `mark` is `less than 0` or `greater than 100`, `print "Invalid Mark"`
+2. if the `mark` is `greater than or equal to 0`, but `less than 50`, `print "Grade D"`
+3. if the `mark` is `greater than or equal to 50`, but `less than 60` `print "Grade C`
+4. if the `mark` is `greater than or equal to 60`, but `less than 80` `print "Grade B`
+5. if the `mark` is `greater than or equal to 80`, but `less than 90` `print "Grade A`
+6. if the `mark` is `greater than or equal to 90`, `print "Grade A+"`
